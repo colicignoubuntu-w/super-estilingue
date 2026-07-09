15a731636eda579b0dcc4dafd13910156569328d
Gabriel Colicigno <colicignoubuntu@gmail.com>
2026-07-09 15:28:50 -0300

    Adiciona seção About

---
+++ frontend/src/components/home/Gallery.tsx
@@ -0,0 +1,80 @@
+import { Box, Container, Grid, Typography } from "@mui/material";
+import { motion } from "framer-motion";
+
+import gallery1 from "../../assets/images/gallery-1.png";
+import gallery2 from "../../assets/images/gallery-2.png";
+import gallery3 from "../../assets/images/gallery-3.png";
+
+const images = [gallery1, gallery2, gallery3];
+
+function Gallery() {
+  return (
+    <Box
+      sx={{
+        py: 12,
+        bgcolor: "#fff",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Typography
+          color="secondary"
+          sx={{
+            textAlign: "center",
+            fontWeight: "bold",
+            mb: 1,
+          }}
+        >
+          GALERIA
+        </Typography>
+
+        <Typography
+          variant="h3"
+          sx={{
+            textAlign: "center",
+            fontWeight: 800,
+            mb: 8,
+          }}
+        >
+          Viva momentos inesquecíveis
+        </Typography>
+
+        <Grid container spacing={4}>
+          {images.map((image, index) => (
+            <Grid key={index} size={{ xs: 12, md: 4 }}>
+              <motion.div
+                initial={{ opacity: 0, y: 60 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                  duration: 0.7,
+                  delay: index * 0.2,
+                }}
+                viewport={{ once: true }}
+              >
+                <Box
+                  component="img"
+                  src={image}
+                  alt={`Galeria ${index + 1}`}
+                  sx={{
+                    width: "100%",
+                    height: 320,
+                    objectFit: "cover",
+                    borderRadius: 4,
+                    transition: ".35s",
+                    cursor: "pointer",
+                    boxShadow: "0 10px 30px rgba(0,0,0,.18)",
+
+                    "&:hover": {
+                      transform: "scale(1.05)",
+                    },
+                  }}
+                />
+              </motion.div>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Box>
+  );
+}
+
+export default Gallery;
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import Navbar from "../../components/layout/Navbar/Navbar";
 import Hero from "../../components/home/Hero";
 import About from "../../components/home/About";
+import Gallery from "../../components/home/Gallery";
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <Navbar />
       <Hero />
       <About />
+      <Gallery />
     </>
   );
 }
