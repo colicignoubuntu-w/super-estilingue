8bf5a03466148029aaef3f243e1398afcd0a6414
Gabriel Colicigno <colicignoubuntu@gmail.com>
2026-07-08 23:09:56 -0300

    Corrige import não utilizado na Home

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../../components/layout/Navbar/Navbar";
 import Hero from "../../components/home/Hero";
-import videoHome from "../../assets/videos/video-home.mp4";
+
 
 function Home() {
   return (
